b328e7c6b1882e45ed57db7065fa0eedebc5f0ac
Julia Kravets <juliaakravets@gmail.com>
2021-03-10 14:39:35 -0500

    added planes to each side of the cube

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+// create a canvas with text (and other components maybe?) and use as a Texture on a plane
+// onClick sets it as active, and displays "click me" button
+// clicking on the button will render a new component (or modal) with page content
+
 import React, { useRef, useState } from "react";
-import * as THREE from "three"
+import * as THREE from "three";
 import { Canvas, useFrame, extend, useThree } from "react-three-fiber";
 import { useSpring, a } from "react-spring/three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import {softShadows} from "@react-three/drei";
+// import About from "./components/About";
 
 extend({ OrbitControls });
 
@@ -24,48 +30,52 @@ const Controls = () => {
 	);
 };
 
-const Plane = ({
-	name,
-	// color = "0xffff00",
-	rotation = [0, 0, 0],
-	position = [0, 0, -.5009],
-}) => {
-  const [hovered, setHovered] = useState(false);
-  const props = useSpring({
-		color: hovered ? "hotpink" : "orange",
+const Plane = ({ name, rotation, position }) => {
+	const [hovered, setHovered] = useState(false);
+	const [active, setActive] = useState(false);
+	const [activePage, setActivePage] = useState(null);
+
+	const props = useSpring({
+		color: hovered || active ? "hotpink" : "orange",
 	});
 
-  return (
-		<mesh
+	const handleClick = () => {
+		setActive(!active);
+		setActivePage(name);
+		console.log(activePage); //figure out how to not select two planes
+	};
+
+	return (
+		<a.mesh
 			name={name}
 			position={position}
 			rotation={rotation}
 			onPointerOver={() => setHovered(true)}
 			onPointerOut={() => setHovered(false)}
+			onClick={handleClick}
 		>
 			<planeBufferGeometry attach="geometry" args={[1, 1, 1]} />
-			<a.meshBasicMaterial
+			<a.meshStandardMaterial
 				attach="material"
 				color={props.color}
 				side={THREE.DoubleSide}
 			/>
-		</mesh>
-	);};
+		</a.mesh>
+	);
+};
 
-const Box = (planeProps) => {
+const Box = () => {
 	const [hovered, setHovered] = useState(false);
 	const [active, setActive] = useState(false);
 	const mesh = useRef(null);
 
-  // const { tagName: Tag, className, variant, children } = planeProps;
-
 	const props = useSpring({
-		// scale: active ? [2, 2, 2] : [1, 1, 1],
-		color: hovered ? "hotpink" : "orange",
+		// scale: active ? [4, 2, 1] : [1, 1, 1],
+		color: hovered || active ? "hotpink" : "orange",
 	});
 
 	useFrame(() =>
-		hovered
+		hovered || active
 			? (mesh.current.rotation.y += 0)
 			: (mesh.current.rotation.y += 0.005)
 	);
@@ -80,34 +90,35 @@ const Box = (planeProps) => {
 		>
 			<Controls />
 			<group>
-				<Plane name="front" />
+				<Plane name="front" position={[0, 0, -0.5009]} rotation={[0, 0, 0]} />
+				<Plane
+					name="back"
+					position={[0, 0, 0.5009]}
+					rotation={[0, 0, Math.PI / 2]}
+				/>
 				<Plane
 					name="left"
-					color={0x00ff00}
-					position={[-0.5009, 0, -0.001]}
+					position={[-0.5009, 0, -0.0001]}
 					rotation={[0, Math.PI / 2, 0]}
 				/>
 				<Plane
 					name="right"
-					color={0x00ffff}
-					position={[0.5009, 0, -0.001]}
+					position={[0.5009, 0, -0.0001]}
 					rotation={[0, Math.PI / 2, 0]}
 				/>
 				<Plane
 					name="bottom"
-					color={0xffccff}
-					position={[0, -0.5009, -0.001]}
+					position={[0, -0.5009, -0.0001]}
 					rotation={[Math.PI / 2, 0, 0]}
 				/>
 				<Plane
 					name="top"
-					color={0xccccff}
-					position={[0, 0.5009, -0.001]}
+					position={[0, 0.5009, -0.0001]}
 					rotation={[Math.PI / 2, 0, 0]}
 				/>
 			</group>
 			<boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-			<a.meshStandardMaterial attach="material" color={props.color} />
+			<a.meshStandardMaterial wireframe attach="material" color={props.color} />
 		</a.mesh>
 	);
 };
@@ -115,23 +126,17 @@ const Box = (planeProps) => {
 export default function App() {
 	return (
 		<>
+			<div>
+				<h1>hi</h1>
+			</div>
 			<Canvas colorManagement camera={{ position: [-5, 2, 10], fov: 10 }}>
 				<ambientLight intensity={0.3} />
-				<directionalLight
-					position={[0, 10, 5]}
-					intensity={1}
-					shadow-mapSize-width={1024}
-					shadow-mapSize-height={1024}
-					shadow-camera-far={50}
-					shadow-camera-left={-10}
-					shadow-camera-right={10}
-					shadow-camera-top={10}
-					shadow-camera-bottom={-10}
-				/>
+				<directionalLight position={[0, 10, 5]} intensity={1} />
 				<pointLight position={[-10, 0, -20]} intensity={0.5} />
 				<pointLight position={[0, -10, 0]} intensity={1.5} />
 				<Box />
 			</Canvas>
+			{/* <About show={showModal} /> */}
 		</>
 	);
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Navbar from "./components/Navbar"
+// import Navbar from "./components/Navbar"
 
 ReactDOM.render(
   <React.StrictMode>
-    <Navbar />
+    {/* <Navbar /> */}
     <App />
   </React.StrictMode>,
   document.getElementById('root')
