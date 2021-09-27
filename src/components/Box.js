6f8393371206155e3d30645ab4d8e8bd95901446
Julia Kravets <julia@onesandzerosai.com>
2021-09-27 14:20:17 -0400

    breakpoints

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,150 +1,150 @@
-import React, { useRef, useState } from "react";
-import * as THREE from "three";
-import { useFrame, extend, useThree } from "react-three-fiber";
-import { useSpring, a } from "react-spring/three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { Text } from "@react-three/drei";
-import { useStore } from "../utils/Context";
+import React, { useRef, useState } from 'react';
+import * as THREE from 'three';
+import { useFrame, extend, useThree } from 'react-three-fiber';
+import { useSpring, a } from 'react-spring/three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { Text } from '@react-three/drei';
+import { useStore } from '../utils/Context';
 
 extend({ OrbitControls });
 
 const Controls = () => {
-	const controls = useRef();
-	const { camera, gl } = useThree();
+  const controls = useRef();
+  const { camera, gl } = useThree();
 
-	return (
-		<orbitControls
-			ref={controls}
-			autoRotate
-			args={[camera, gl.domElement]}
-		></orbitControls>
-	);
+  return (
+    <orbitControls
+      ref={controls}
+      autoRotate
+      args={[camera, gl.domElement]}
+    ></orbitControls>
+  );
 };
 
 const Plane = ({ name, rotation, position }) => {
-	const {
-		openAbout,
-		openStack,
-		openProjects,
-		openContact,
-		setOpenAbout,
-		setOpenStack,
-		setOpenProjects,
-		setOpenContact,
-	} = useStore();
+  const {
+    openAbout,
+    openStack,
+    openProjects,
+    openContact,
+    setOpenAbout,
+    setOpenStack,
+    setOpenProjects,
+    setOpenContact,
+  } = useStore();
 
-	const [hovered, setHovered] = useState(false);
-	const [active, setActive] = useState(false);
+  const [hovered, setHovered] = useState(false);
+  const [active, setActive] = useState(false);
 
-	const props = useSpring({
-		color: hovered || active ? "hotpink" : "#f4acb7",
-	});
+  const props = useSpring({
+    color: hovered || active ? 'hotpink' : '#f4acb7',
+  });
 
-	const handleClick = (e) => {
-		e.stopPropagation();
+  const handleClick = (e) => {
+    e.stopPropagation();
     setActive(!active);
-		if (active && name === "About") setOpenAbout(!openAbout);
-		if (active && name === "Stack") setOpenStack(!openStack);
-		if (active && name === "Projects") setOpenProjects(!openProjects);
-		if (active && name === "Contact") setOpenContact(!openContact);
-	};
+    if (active && name === 'About') setOpenAbout(!openAbout);
+    if (active && name === 'Stack') setOpenStack(!openStack);
+    if (active && name === 'Projects') setOpenProjects(!openProjects);
+    if (active && name === 'Contact') setOpenContact(!openContact);
+  };
 
-	return (
-		<a.mesh
-			name={name}
-			position={position}
-			rotation={rotation}
-			onPointerOver={() => setHovered(true)}
-			onPointerOut={() => setHovered(false)}
-			onPointerDown={handleClick}
-		>
-			<planeBufferGeometry attach="geometry" args={[1, 1, 1]} />
-			<a.meshStandardMaterial
-				attach="material"
-				color={props.color}
-				side={THREE.DoubleSide}
-				polygonOffset={true}
-				polygonOffsetFactor={-1}
-				polygonOffsetUnits={-1}
-			/>
-			<Text
-				color="#fff8dc"
-				anchorX="center"
-				anchorY="middle"
-				depthOffset={-2}
-				materialType="meshStandardMaterial"
-				font="https://fonts.gstatic.com/s/raleway/v14/1Ptrg8zYS_SKggPNwK4vaqI.woff"
-				maxWidth={1}
-				fontSize={0.17}
-				textAlign="center"
-			>
-				{`${name}`.toUpperCase()}
-			</Text>
-		</a.mesh>
-	);
+  return (
+    <a.mesh
+      name={name}
+      position={position}
+      rotation={rotation}
+      onPointerOver={() => setHovered(true)}
+      onPointerOut={() => setHovered(false)}
+      onPointerDown={handleClick}
+    >
+      <planeBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <a.meshStandardMaterial
+        attach="material"
+        color={props.color}
+        side={THREE.DoubleSide}
+        polygonOffset={true}
+        polygonOffsetFactor={-1}
+        polygonOffsetUnits={-1}
+      />
+      <Text
+        color="#fff8dc"
+        anchorX="center"
+        anchorY="middle"
+        depthOffset={-2}
+        materialType="meshStandardMaterial"
+        font="https://fonts.gstatic.com/s/raleway/v14/1Ptrg8zYS_SKggPNwK4vaqI.woff"
+        maxWidth={1}
+        fontSize={0.17}
+        textAlign="center"
+      >
+        {`${name}`.toUpperCase()}
+      </Text>
+    </a.mesh>
+  );
 };
 
 export default function Box() {
-	const [hovered, setHovered] = useState(false);
-	const [active, setActive] = useState(false);
-	const mesh = useRef(null);
+  const [hovered, setHovered] = useState(false);
+  const [active, setActive] = useState(false);
+  const mesh = useRef(null);
 
-	const props = useSpring({
-		color: hovered || active ? "hotpink" : "#f4acb7",
-	});
+  const props = useSpring({
+    color: hovered || active ? 'hotpink' : '#f4acb7',
+  });
 
-	let date = new Date();
-	let year = date.getFullYear();
+  let date = new Date();
+  let year = date.getFullYear();
 
-	useFrame(() =>
-		hovered || active
-			? (mesh.current.rotation.y += 0)
-			: (mesh.current.rotation.y += 0.005)
-	);
+  useFrame(() =>
+    hovered || active
+      ? (mesh.current.rotation.y += 0)
+      : (mesh.current.rotation.y += 0.005),
+  );
 
-	return (
-		<a.mesh
-			ref={mesh}
-			onPointerOver={() => setHovered(true)}
-			onPointerOut={() => setHovered(false)}
-			onClick={() => setActive(!active)}
-			// scale={props.scale}
-		>
-			<Controls />
-			<group>
-				<Plane
-					name="Projects"
-					position={[0, 0, -0.5009]}
-					rotation={[0, Math.PI, 0]}
-				/>
-				<Plane
-					name="Stack"
-					position={[0, 0, 0.5009]}
-					rotation={[Math.PI, -Math.PI, Math.PI]}
-				/>
-				<Plane
-					name="About"
-					position={[-0.5009, 0, -0.0001]}
-					rotation={[0, -Math.PI / 2, 0]}
-				/>
-				<Plane
-					name="Contact"
-					position={[0.5009, 0, -0.0001]}
-					rotation={[0, Math.PI / 2, 0]}
-				/>
-				<Plane
-					name={`© ${year}`}
-					position={[0, -0.5009, -0.0001]}
-					rotation={[Math.PI / 2, 0, Math.PI / 2]}
-				/>
-				<Plane
-					name="Julia Kravets"
-					position={[0, 0.5009, -0.0001]}
-					rotation={[Math.PI / 2, Math.PI, Math.PI / 2]}
-				/>
-			</group>
-			<boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-			<a.meshStandardMaterial wireframe attach="material" color={props.color} />
-		</a.mesh>
-	);
+  return (
+    <a.mesh
+      ref={mesh}
+      onPointerOver={() => setHovered(true)}
+      onPointerOut={() => setHovered(false)}
+      onClick={() => setActive(!active)}
+      // scale={props.scale}
+    >
+      <Controls />
+      <group>
+        <Plane
+          name="Projects"
+          position={[0, 0, -0.5009]}
+          rotation={[0, Math.PI, 0]}
+        />
+        <Plane
+          name="Stack"
+          position={[0, 0, 0.5009]}
+          rotation={[Math.PI, -Math.PI, Math.PI]}
+        />
+        <Plane
+          name="About"
+          position={[-0.5009, 0, -0.0001]}
+          rotation={[0, -Math.PI / 2, 0]}
+        />
+        <Plane
+          name="Contact"
+          position={[0.5009, 0, -0.0001]}
+          rotation={[0, Math.PI / 2, 0]}
+        />
+        <Plane
+          name={`© ${year}`}
+          position={[0, -0.5009, -0.0001]}
+          rotation={[Math.PI / 2, 0, Math.PI / 2]}
+        />
+        <Plane
+          name="Julia Kravets"
+          position={[0, 0.5009, -0.0001]}
+          rotation={[Math.PI / 2, Math.PI, Math.PI / 2]}
+        />
+      </group>
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <a.meshStandardMaterial wireframe attach="material" color={props.color} />
+    </a.mesh>
+  );
 }
--- src/utils/Theme.js
+++ src/utils/Theme.js
@@ -5,9 +5,9 @@ const customBreakpointValues = {
   values: {
     xs: 0,
     sm: 600,
-    md: 960,
-    lg: 1280,
-    xl: 1920,
+    md: 900,
+    lg: 1200,
+    xl: 1536,
   },
 };
 
